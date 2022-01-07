a8af7d78f6a37941e418ffae35225e5900781b1b
rodrigolange <52110364+rodrigolange@users.noreply.github.com>
2022-01-06 22:03:03 -0300

    Cliente lado robodog.

--- ControleRobodog/Robodog/Robodog.py
+++ ControleRobodog/Robodog/Robodog.py
@@ -1,4 +1,10 @@
 import ServidorControleRobodog
+import Hardware.Pernas as Pernas
+import Hardware.Sonar as Sonar
+
+p = Pernas.Pernas()
+s = Sonar.Sonar(23, 24)
+
 
 print("Iniciando servidor de controle do Robodog")
 Robodog = ServidorControleRobodog.ServidorControleRobodog()
