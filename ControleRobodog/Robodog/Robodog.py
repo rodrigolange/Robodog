8505410905e2e819908ebe069f4e263447a5e213
rodrigolange <52110364+rodrigolange@users.noreply.github.com>
2022-01-06 22:19:49 -0300

    Cliente lado robodog.

--- ControleRobodog/Robodog/Robodog.py
+++ ControleRobodog/Robodog/Robodog.py
@@ -1,11 +1,25 @@
+import threading
+
 import ServidorControleRobodog
 import Hardware.Pernas as Pernas
 import Hardware.Sonar as Sonar
+from queue import Queue
+
+filaMovimentos = Queue()
+
 
 p = Pernas.Pernas()
 s = Sonar.Sonar(23, 24)
 
+def consumer(in_q):
+    while True:
+        # Get some data
+        data = in_q.get()
+        print(data)
+
 
 print("Iniciando servidor de controle do Robodog")
-Robodog = ServidorControleRobodog.ServidorControleRobodog()
+Robodog = ServidorControleRobodog.ServidorControleRobodog(filaMovimentos)
 
+t1 = threading.Thread(target=consumer, args =(filaMovimentos, ))
+t1.start()
--- ControleRobodog/Robodog/ServidorControleRobodog.py
+++ ControleRobodog/Robodog/ServidorControleRobodog.py
@@ -18,11 +18,17 @@ class ServidorControleRobodog:
                     data = conn.recv(1024)
                     if not data or data.decode() == -10:
                         break
-                    print(data.decode())
 
-    def __init__(self):
+                    listaPosicoes = data.decode().split(",")
+                    self.filaMovimentos.put(listaPosicoes)
+                    #print(data.decode())
+                    print(listaPosicoes)
 
-        print("Criando a thread")
+    def __init__(self, filaMovimentos):
+
+        self.filaMovimentos = filaMovimentos
+
+        print("Criando servidor socket")
         x = threading.Thread(target=self.socketServidor)
-        print("Iniciando a thread")
+        print("Iniciando servidor socket")
         x.start()
