c59dc99cd251171da1e74fd00e3a818ec90a05a6
rodrigolange <52110364+rodrigolange@users.noreply.github.com>
2022-01-06 23:49:31 -0300

    Cliente lado robodog.

--- ControleRobodog/Robodog/Robodog.py
+++ ControleRobodog/Robodog/Robodog.py
@@ -1,4 +1,5 @@
 import threading
+import time
 
 import ServidorControleRobodog
 import Hardware.Pernas as Pernas
@@ -11,18 +12,40 @@ filaMovimentos = Queue()
 p = Pernas.Pernas()
 s = Sonar.Sonar(23, 24)
 
-def consumer(in_q):
-    while True:
-        # Get some data
-        data = in_q.get()
-        print(data)
+posicoesIniciais = [90, 0, 150, 90, 180, 40, 90, 0, 150, 90, 180, 40]
+posicoesF = [80, 0, 150, 100, 180, 40, 80, 50, 120, 100, 130, 70]  # levanta frente
+posicoesPernas = [90, 0, 150, 90, 180, 40, 90, 0, 150, 90, 180, 40]
 
+
+def movimentoRobodog(in_q):
+
+    #p.moverPernasLento10(posicoesIniciais)
+    time.sleep(1)
+
+    #p.moverPernasLento10(posicoesF)
+    time.sleep(1)
+
+    posicoesPernas = posicoesF
+
+    while True:
+        data = in_q.get()
         if "-10.0" in data:
             break
+
+        dados = [float(x) for x in data]
+
+        posicoesPernas[10] = posicoesPernas[10] + int(dados[1])
+        #p.moverPernasLento10(posicoesPernas)
+
+        print(posicoesPernas[10])
+
+
+
+
 
 
 print("Iniciando servidor de controle do Robodog")
 Robodog = ServidorControleRobodog.ServidorControleRobodog(filaMovimentos)
 
-t1 = threading.Thread(target=consumer, args =(filaMovimentos, ))
-t1.start()
+tMovimentoRobodog = threading.Thread(target=movimentoRobodog, args =(filaMovimentos, ))
+tMovimentoRobodog.start()
