88117960235dde9cb125cab3e4c2a59f28b1699b
rodrigolange <52110364+rodrigolange@users.noreply.github.com>
2022-01-06 21:11:56 -0300

    Cliente lado robodog.

--- ControleRobodog/Robodog.py
+++ ControleRobodog/Robodog.py
@@ -3,5 +3,6 @@ import ServidorControleRobodog
 HOST = '127.0.0.1'
 PORT = 65432
 
+print("Iniciando servidor de controle do Robodog")
 Robodog = ServidorControleRobodog.ServidorControleRobodog(HOST, PORT)
 
--- ControleRobodog/ServidorControleRobodog.py
+++ ControleRobodog/ServidorControleRobodog.py
@@ -6,6 +6,7 @@ class ServidorControleRobodog:
 
     def socketServidor(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            print("criando socket")
             s.bind((self.HOST, self.PORT))
             s.listen()
             conn, addr = s.accept()
@@ -22,5 +23,7 @@ class ServidorControleRobodog:
         self.HOST = host
         self.PORT = port
 
+        print("Criando a thread")
         x = threading.Thread(target=self.socketServidor)
+        print("Iniciando a thread")
         x.start()
